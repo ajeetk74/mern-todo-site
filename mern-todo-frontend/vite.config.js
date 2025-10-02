5b80a2ce714238e65c4622ca8047568f69d64ddc
AJEET KUMAR <mjajeetkumar1919991@gmail.com>
2025-10-02 10:00:24 +0530

    Update vite.config.js

--- mern-todo-frontend/vite.config.js
+++ mern-todo-frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mern-todo-site/',
+  base: '/mern-todo-site/', // important for GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
-      '/todos': 'http://localhost:5001',
+      '/todos': 'http://localhost:5001', // works only in local dev
     },
   },
 })
