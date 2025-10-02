67aea5b1ab5628c29ae4cd98e98e12404fee5e06
AJEET KUMAR <mjajeetkumar1919991@gmail.com>
2025-10-02 14:46:26 +0530

    Update api.js

--- mern-todo-frontend/src/services/api.js
+++ mern-todo-frontend/src/services/api.js
@@ -1,8 +1,23 @@
-import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5001' });
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+import axios from "axios";
+
+// Base URL of backend
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5001";
+
+// Create axios instance
+const api = axios.create({
+  baseURL: API_BASE,
 });
+
+// Add a request interceptor to include JWT automatically
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
